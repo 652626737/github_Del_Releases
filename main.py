67afe67cfe7f795c440699983a3a31c7bec367ec
liyong <liyong@macbookAir.local>
2025-10-21 02:06:15 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+print(3>2)
+print(3<2 )
+
+print(3>2 and 5>1)  #与运算 只有全部为true 结果才为True
+
+print(3>4 and 5>1) #与运算 只有全部为true 结果才为True
+
+print('3>2 or 4<1')
+print(3>2 or 4<1) #或运算 只要有一个为True 结果就为True
+
+print( not 3>2) #非运算 运算结果取反
+
+
+if __name__ == '__main__':
+    pass
